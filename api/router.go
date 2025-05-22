16338786e32149d28fb0f616fed95faf61be6dc3
Liborio <liborioleite@gmail.com>
2025-05-22 15:19:38 -0300

    add cors

--- api/router.go
+++ api/router.go
@@ -2,12 +2,20 @@ package api
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func InitializeFiber() {
 	app := fiber.New(fiber.Config{
 		AppName: "Mentoring Api",
 	})
+
+	// Configura o middleware CORS
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "*", // Permite requisições de qualquer origem
+		AllowMethods: "GET,POST,PUT,DELETE,OPTIONS",
+		AllowHeaders: "Origin, Content-Type, Accept, Authorization",
+	}))
 
 	InitializeRoutes(app)
 
